cf775a5b8aa6e3411a78793a06002af3a56aada7
Zenjutahi <zenjutahi@gmail.com>
2019-06-08 23:38:28 +0300

    [chore] - debug cloudinary error

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,4 @@
-Clodinary.config do |config|
+Cloudinary.config do |config|
   config.cloud_name = ENV["cloudinary_cloud_name"]
   config.api_key = ENV["cloudinary_api_key"]
   config.api_secret = ENV["cloudinary_api_secret"]
