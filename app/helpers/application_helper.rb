5a5cae3a1a596acffe47f2c5dec2abc1f6a5ac59
Zenjutahi <zenjutahi@gmail.com>
2019-06-10 15:42:27 +0300

    [chore] - ad css to style post index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def avatar_url user
     require 'digest/md5'
-    return user.image.gsub!("_normal", "") if user.image
+    return user.image if user.image
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar#{gravatar_id}.jpg"
   end
