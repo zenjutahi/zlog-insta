327c7d612b9558fe0cd991edbd29d50965ba0d0e
Zenjutahi <zenjutahi@gmail.com>
2019-06-18 09:54:57 +0300

    [chore] -style and configre like post section

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
-    resources :likes, only: [:create, :destroy, :index, :show, :new, :edit, :update], shallow: true
+    resources :likes, only: [:create, :destroy], shallow: true
   end
 end
