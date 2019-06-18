845ad0ea2e3a4f80279c2230da1d02b4367ec32d
Zenjutahi <zenjutahi@gmail.com>
2019-06-18 09:39:33 +0300

    [chore] -style and configre like post section

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ApplicationRecord
     Post.find_by(user_id: user.id, id: id)
   end
   
-  def is_liked users
+  def is_liked user
     Like.find_by(user_id: user.id, post_id: id)
   end
 end
