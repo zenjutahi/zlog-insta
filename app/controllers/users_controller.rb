3fe3061ddef29ffef37e42d05d7624e6f2b4ea99
Zenjutahi <zenjutahi@gmail.com>
2019-06-19 11:06:10 +0300

    [chore] -debug bookmarks functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos, :likes, :comments)
-    @saved = Post.joins(:bookmarks).includes(:photos, :likes, :comments)
+    @saved = Post.joins(:bookmarks).
+      includes(:photos, :likes, :comments) if @user == current_user
   end
 end
