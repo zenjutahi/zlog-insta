1640d1083f191b6f878ed905f80e0fcff292c627
Zenjutahi <zenjutahi@gmail.com>
2019-06-19 10:54:57 +0300

    [faeture] - add search component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.search(params[:term])
+    respond_to :js
+  end
+    
+  
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.includes(:photos, :likes, :comments)
+    @saved = Post.joins(:bookmarks).includes(:photos, :likes, :comments)
   end
 end
