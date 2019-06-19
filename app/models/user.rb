9f036aef5a30b6324dbdce72f38d4d1b30e36aa3
Zenjutahi <zenjutahi@gmail.com>
2019-06-19 10:55:23 +0300

    [faeture] - add search component

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,24 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0, 20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image.gsub!("_normal", "")# assuming the user model has an image
-    user.uid = auth.uid
-    user.provider = auth.provider
-    # If you are using confirmable and the provider(s) you use validate emails, 
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image.gsub!("_normal", "")# assuming the user model has an image
+      user.uid = auth.uid
+      user.provider = auth.provider
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
-end
+  
+  def self.search(term)
+    if term
+      where('name LIKE ?', "%#{term}%")
+    else
+      nil
+    end
+  end
 end
